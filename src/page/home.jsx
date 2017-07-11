57ba9fa8ea7df962d624beaa48a9573ee4eaaa49
秦磊磊 <qinlei350@126.com>
2017-07-11 16:46:27 +0800

    tests

--- src/page/home.jsx
+++ src/page/home.jsx
@@ -4,6 +4,6 @@
 // import { Prompt } from 'react-router-dom'
 import styles from './home.css'
 
-const HomePage = () => <div><p className={styles.testClass}>Home </p></div>
+const HomePage = () => <div><p className={styles.testClass}>Home11 </p></div>
 
 export default HomePage
